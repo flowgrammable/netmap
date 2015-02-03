47bf46158cd6e2081741d575ea72e76a8f7a1551
Luke McHale <lam89@tamu.edu>
2015-02-02 18:24:36 -0600

    updated vagrant box install script.  Added Wall flag to netmap makefile

--- configure-vagrant.sh
+++ configure-vagrant.sh
@@ -2,12 +2,14 @@
 
 rsync -a /vagrant/netmap_source ~/
 cd ~/netmap_source/LINUX
-./configure --kernel-dir=/home/vagrant/kernel/build --kernel-sources=/usr/src/linux-3.14.28 --install-mod-path=/ --driver-suffix='nm-'
+#./configure --kernel-dir=/home/vagrant/kernel/build --kernel-sources=/usr/src/linux-3.14.28 --install-mod-path=/ --driver-suffix='-nm'
+./configure --driver-suffix='-nm' --disable-vale
 make
 sudo make install
 cd ../examples
 make
+rm -rf ~/netmapbin
 mkdir ~/netmapbin
 cp bridge ~/netmapbin
 cp pkt-gen ~/netmapbin
-
+echo 'Done!'
